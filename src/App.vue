d21a9c610f6d2c28a234b84e7967b4d8d79a92f0
andi <andiks2018@gmail.com>
2022-06-25 10:58:44 +0700

    FirstCommit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">SIMPLE VUE JS APP</h5>
+        <div class="row mb-3">
+          <div class="col-10">
+            <input @keyup.enter="add" type="text" class="form-control" v-model="todo" />
+          </div>
+          <div class="col">
+            <button class="btn btn-primary" @click="add">Add</button>
+          </div>
+        </div>
+        <div class="row">
+           <list :todos="todos" @deleteTodo="deleteTodo"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./components/List.vue";
+
+export default {
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+  methods : {
+    add(){
+      this.todos.unshift(this.todo)
+      this.todo=""
+    },
+    deleteTodo(todoIndex){
+      this.todos = this.todos.filter((item, index)=>{
+        if (index!=todoIndex){
+          return item;
+        }
+      })
+    }
+  },
+  components: {
+    List,
+  },
+};
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list-group">
+    <li v-for="(todo, index) in todos" :key="todo" class="list-group-item">
+      <div class="row">
+        <div class="col">
+          <span>{{ todo }}</span>
+        </div>
+        <div class="col-auto">
+          <div class="row">
+            <div class="col">
+              <button @click="deleteTODO(index)" class="btn btn-danger">X</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    deleteTODO(index) {
+      this.$emit("deleteTodo", index);
+    },
+  },
+};
+</script>
